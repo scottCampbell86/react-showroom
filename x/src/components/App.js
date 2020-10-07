79ce62344fc702997429fa1864eedcb6cffd406c
Scott Campbell <stc9486@gmail.com>
2020-10-07 18:48:22 -0400

    async/await

--- x/src/components/App.js
+++ x/src/components/App.js
@@ -5,13 +5,14 @@ import SearchBar from './SearchBar';
 
 class App extends React.Component {
 
-  onSearchSubmit(term) {
-    axios.get('https://api.unsplash.com/search/photos', {
+  async onSearchSubmit(term) {
+    const res = await axios.get('https://api.unsplash.com/search/photos', {
       params: {query: term},
       headers: {
         Authorization: 'Client-ID 1ukgpv-8ju2FjFZfOHqlL6gN_KEG2vrOyncYDOx9J_g'
       }
-    })
+    });
+    console.log(res.data.results);
   }
 
   render () {    
