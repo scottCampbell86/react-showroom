433b3244a71a4b23dd2899fa47cd299c3ee8e07c
Scott Campbell <stc9486@gmail.com>
2020-10-08 18:26:05 -0400

    ok

--- x/src/components/App.js
+++ x/src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import unsplashAxios from '../api/unsplashAxios'
+import unsplashAxios from '../api/unsplashAxios';
+import ImageList from './ImageList'
 import SearchBar from './SearchBar';
 
 
@@ -24,7 +25,7 @@ class App extends React.Component {
     return (
       <div className='ui container' style={{marginTop: '10px'}}>
         <SearchBar onSearchSubmit={this.onSearchSubmit} />
-        Found: {this.state.images.length} images
+        <ImageList images={this.state.images}/>
       </div>
     )
   }
